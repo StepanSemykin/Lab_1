275e701b6f8b237c19ea477e15603d190007c63e
StepanSemykin <semykin.2003@mail.ru>
2022-09-28 23:17:56 +0400

    Проверка URL на действительность

--- Images.py/Images.py
+++ Images.py/Images.py
@@ -6,12 +6,13 @@ import os
 from tqdm import tqdm
 from bs4 import BeautifulSoup as bs
 from urllib.parse import urljoin, urlparse
+import random
 
 directory_dataset = "D:\VS Code project\Images.py\dataset"
 directory_rose = "D:\VS Code project\Images.py\dataset\rose"
 directory_tulip = "D:\VS Code project\Images.py\dataset\tulip"
 
-def directory():    
+def directory():   
     if not os.path.isdir("dataset"):
         os.mkdir(directory_dataset)      
     if not os.path.isdir("rose"):
@@ -19,5 +20,10 @@ def directory():
     if not os.path.isdir("tulip"):
         os.mkdir(directory_tulip)
         
-     
-     
+url = "https://yandex.ru/images/search?text=rose"
+             
+def valid(url):
+    parse = urlparse(url)
+    return bool(parse.scheme) and bool(parse.netloc)     
+
+
